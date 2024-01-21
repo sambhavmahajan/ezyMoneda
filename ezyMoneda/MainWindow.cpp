44b4270539c779ecd3d064d3eabc7430d7560683
Sambhav Mahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-01-22 00:25:32 +0530

    Added winform

---
+++ ezyMoneda/MainWindow.cpp
@@ -0,0 +1,17 @@
+#include "MainWindow.h"
+
+using namespace System;
+
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+void Main(array<String^>^ args)
+
+{
+
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	ezyMoneda::MainWindow form; Application::Run(% form);
+
+}
