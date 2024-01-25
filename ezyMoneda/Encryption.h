4d7b903b7c8f5ed32304bad3e1673413331221e6
Sambhav Mahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-01-25 23:10:05 +0530

    Update Encryption.h
    Typo fix.

--- ezyMoneda/Encryption.h
+++ ezyMoneda/Encryption.h
@@ -4,46 +4,17 @@
 #include<string>
 #define ENCRYPTIONKEY 90767
 
-std::string encrypt(std::string& password , std::string& key  );
-std::string encryptedpassword = "";
+std::string encrypt(std::string& password , std::string& key  ){
+    std::string encryptedpassword = "";
 
-for (size_t i = 0; i < password.length(); ++i) {
-    char currentLetter = password[i];
-
-    char keyLetter = key[i % key.length()];
-
-
-    char encryptedLetter = currentLetter ^ keyLetter;
-
-    encryptedPassword += encryptedLetter;
-}
-return encryptedPassword;
-
-int main() {
-    std::string originalPassword;
-    std::cout << "Enter the original password: ";
-    std::cin >> originalPassword;
-
-    std::string encryptionKey = "EncryptionKey";
-
-    std::string userPassword;
-    std::cout << "Enter your password: ";
-    std::cin >> userPassword;
-
-  
-    std::string encryptedOriginalPassword = encrypt(originalPassword, encryptionKey);
-    std::string encryptedUserPassword = encrypt(userPassword, encryptionKey);
-
-    if (encryptedUserPassword == encryptedOriginalPassword) {
-        std::cout << "Password is correct!" << std::endl;
-    }
-    else {
-        std::cout << "Password is incorrect!" << std::endl;
-    }
-
-
-    return 0;
-}
-
+    for (size_t i = 0; i < password.length(); ++i) {
+        char currentLetter = password[i];
     
+        char keyLetter = key[i % key.length()];
+        char encryptedLetter = currentLetter ^ keyLetter;
+    
+        encryptedPassword += encryptedLetter;
+    }
+    return encryptedPassword;
+}  
 #endif
