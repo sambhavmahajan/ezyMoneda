f18abf0b13ff89548ded62120c8154aba06e1430
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-01-25 15:45:44 +0530

    added _time variable

--- ezyMoneda/Transaction.h
+++ ezyMoneda/Transaction.h
@@ -1,11 +1,12 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
-
+#include <ctime>
 enum transactionType {
 	DEBIT, CREDIT
 };
 class Transaction {
 private:
-	size_t _id;
+	size_t id;
+	time_t time;
 };
 #endif
