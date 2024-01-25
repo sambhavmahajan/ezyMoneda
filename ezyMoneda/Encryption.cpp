91de32d3dfac9ee15a166c36dbac104915e4754a
vinesh kumar goswami <VKG2005@users.noreply.github.com>
2024-01-25 22:19:58 +0530

    create the loop through each letter of password

--- ezyMoneda/Encryption.cpp
+++ ezyMoneda/Encryption.cpp
@@ -4,3 +4,8 @@ std::string encrypt(std::string& password)
 {
     return std::string();
 }
+
+std::string encrypt(std::string& password, std::string& key)
+{
+    return std::string();
+}
--- ezyMoneda/Encryption.h
+++ ezyMoneda/Encryption.h
@@ -4,9 +4,11 @@
 #include<string>
 #define ENCRYPTIONKEY 90767
 
-std::string encrypt(std::string& password);
+std::string encrypt(std::string& password , std::string& key  );
+std::string encryptedpassword = "";
 
-extern std::string encryptedpassword;
+for (size_t i = 0; i < password.length(); ++i) {
+    char currentLetter = password[i];
 
-
+    
 #endif
