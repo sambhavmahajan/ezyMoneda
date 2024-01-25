3ea8c05dfc19deb9116b4f6db0a6a512c6f4ab35
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-01-25 22:14:03 +0530

    isValidCreditCard implimented.

--- ezyMoneda/autheticator.cpp
+++ ezyMoneda/autheticator.cpp
@@ -5,4 +5,38 @@ bool isValidCreditCard(size_t cardnum)
 	if (cardnum == 0) {
 		return false;
 	}
+	int temp = cardnum;
+	int digits = (int)(log10(cardnum) + 1);
+	if (digits != 16) {
+		return false; //visa cardnum have 16 digit length
+	}
+	int* arr = new int[digits];
+	int i = digits - 1;
+	for (; i >= 0; i--)
+	{
+		arr[i] = temp % 10;
+		temp /= 10;
+	}
+	if (arr[0] != 4)
+	{
+		delete[] arr;
+		return false; //visa cardnum starts with 4
+	}
+	int sum = 0;
+	for (i = 0; i < digits; i++)
+	{
+		if (i % 2 == 1)
+		{
+			//every second digit is doubled.
+			arr[i] *= 2;
+			if (arr[i] > 9)
+			{
+				temp = arr[i] % 10 + 1; //if number greator than 10, tens place digit is always 1
+				arr[i] = temp;
+			}
+		}
+		sum += arr[i];
+	}
+	delete[] arr;
+	return sum % 10 == 0;
 }
