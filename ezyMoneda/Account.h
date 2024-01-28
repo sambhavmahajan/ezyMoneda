786a4b3df3060d0a0d12e6c77e1b55ad3adb1463
RajAstha1 <157282933+RajAstha1@users.noreply.github.com>
2024-01-28 10:50:32 +0530

    Update Account.h
    creation of account class completed

--- ezyMoneda/Account.h
+++ ezyMoneda/Account.h
@@ -1,5 +1,21 @@
 #ifndef Account_H
 #define Account_H
-class account{
-	
+
+#include <string>
+
+class Account {
+private:
+    int ID;
+    std::string Name;
+    std::string Email;
+    std::string Password;
+    float Balance;
+
+public:
+    std::string getName() const { return Name; }
+    std::string getEmail() const { return Email; }
+    std::string getPassword() const { return Password; }
+    float getBalance() const { return Balance; }
 };
+
+#endif
