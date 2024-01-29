07057e9c944aca11216a1af119d412cfebd85028
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-01-29 19:35:29 +0530

    Completed hash function.

--- ezyMoneda/encryption.cpp
+++ ezyMoneda/encryption.cpp
@@ -1,7 +1,34 @@
 #include "encryption.h"
 
+char convertToAlpha(char c)
+{
+	if (c < 'A' || c>'Z') {
+		c = c % Total_Upper+ ASCII_A;
+	}
+	return c;
+}
+
+
 std::string hash(std::string input)
 {
 	std::string r = "";
-	return std::string();
+	int length = input.length();
+	bool flag = false;
+	int offset = length % 5;
+	while(input.length() > 16)
+	{
+		for (int i = 0; i < input.length() - 1; i++)
+		{
+			input[i] *= (i+offset) ^ input[length - 1] % Total_Upper + ASCII_A;
+			input.pop_back();
+		}
+	}
+	for (int i = 0; i < HASH_LENGTH; i++)
+	{
+		flag = !flag;
+		if (flag) {
+			r += (input[i]*(input[i])*length) % Total_Upper + ASCII_A;
+		}
+	}
+	return r;
 }
--- ezyMoneda/encryption.h
+++ ezyMoneda/encryption.h
@@ -1,5 +1,9 @@
 #ifndef ENCRYPTION_H
 #define ENCRYPTION_H
+#define ASCII_A 65
+#define Total_Upper 25
+#define HASH_LENGTH 16
 #include <string>
+char convertToAlpha(char c);
 std::string hash(std::string input);
 #endif
